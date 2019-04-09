99b30064df0376ee6c261b9470db9565e6852421
aurimas-darguzis <aurimas@darguzis.com>
2019-04-09 13:54:09 +0100

    wip

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -14,16 +14,36 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <Link to='/'>Home</Link>
+                <NavLink
+                  to='/'
+                  exact
+                  activeClassName='my-active'
+                  activeStyle={{
+                    color: '#fa923f',
+                    textDecoration: 'underline'
+                  }}
+                >
+                  Posts
+                </NavLink>
               </li>
               <li>
-                <Link to='/new-post'>New Post</Link>
+                <NavLink
+                  to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quick-submit=true'
+                  }}
+                >
+                  New Post
+                </NavLink>
               </li>
             </ul>
           </nav>
         </header>
-        <Route path='/' exact component={Posts} />
-        <Route path='/new-post' exact component={NewPost} />
+        <Switch>
+          <Route path='/new-post' exact component={NewPost} />
+          <Route path='/' component={Posts} />
+        </Switch>
       </div>
     );
   }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
+import { Route } from 'react-router-dom';
 import Post from '../../../components/Post/Post';
+import FullPost from '../FullPost/FullPost';
+
 import './Posts.css';
 
 export default class Posts extends Component {
@@ -30,11 +33,11 @@ export default class Posts extends Component {
   }
 
   postSelectedHandler = id => {
-    this.setState({ selectedPostId: id });
+    this.props.history.push({ pathname: '/' + id });
   };
 
   render() {
-    let posts;
+    let posts = <p style={{ textAlign: 'center' }} Something went wrong />;
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
         return (
@@ -48,6 +51,11 @@ export default class Posts extends Component {
       });
     }
 
-    return <section className='Posts'>{posts}</section>;
+    return (
+      <div>
+        <section className='Posts'>{posts}</section>
+        <Route path='/:id' exact component={FullPost} />
+      </div>
+    );
   }
 }
